69bad80ca7b62f8ee178dc60a755e91abcb9279b
rdwnrymn <rdwnrymn@gmail.com>
2025-06-25 15:50:32 +0700

    add lsp plugin

--- lua/plugins/fzflua.lua
+++ lua/plugins/fzflua.lua
@@ -8,33 +8,59 @@ return {
 	keys={
 		{ 
 			"<leader>ff", 
-			function() require('fzf-lua').files() end,
-			desc="Find files in current project path"
+			function() 
+                require('fzf-lua').files() 
+            end,
+			desc = "Find files in current project path"
 		},
 		{ 
 			"<leader><leader>", 
-			function() require('fzf-lua').buffers() end,
-			desc="Open a buffers in current project"
+			function() 
+                require('fzf-lua').buffers() 
+            end,
+			desc = "Open buffers in current project"
 		},
 		{ 
 			"<leader>fg", 
-			function() require('fzf-lua').live_grep() end,
-			desc="Live grep in current project"
+			function() 
+                require('fzf-lua').live_grep() 
+            end,
+			desc = "Live grep in current project directory"
+		},
+		{ 
+			"<leader>f/", 
+			function() 
+                require('fzf-lua').lgrep_curbuf() 
+            end,
+			desc = "Live grep in current project directory"
 		},
 		{ 
 			"<leader>f~", 
-			function() require('fzf-lua').files( { cwd = '~/' } ) end,
-			desc="Find file in Home directory"
+			function() 
+                require('fzf-lua').files( { cwd = '~/' } ) 
+            end,
+			desc = "Find file in Home directory"
 		},
 		{ 
 			"<leader>fb", 
-			function() require('fzf-lua').builtin() end,
-			desc="Find all builtin functions in FZF "
+			function() 
+                require('fzf-lua').builtin() 
+            end,
+			desc = "Find all builtin functions in FZF "
 		},
 		{ 
 			"<leader>fk", 
-			function() require('fzf-lua').keymaps() end,
-			desc="Find all keymaps"
+			function() 
+                require('fzf-lua').keymaps() 
+            end,
+			desc = "Find all keymaps"
+		},
+		{ 
+			"<leader>fh", 
+			function() 
+                require('fzf-lua').helptags() 
+            end,
+			desc = "Find helps"
 		}
 	}
 }
