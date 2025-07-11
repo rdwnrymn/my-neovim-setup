cd03b8c85ad66eda9c11b9a2e56547708d9c3164
rdwnrymn <rdwnrymn@gmail.com>
2025-07-11 17:51:12 +0700

    edit stuff, install blink-cmp

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -11,5 +11,10 @@ return {
             javascript = { "prettierd", "prettier", stop_after_first = true },
             typescript = { "prettierd", "prettier", stop_after_first = true },
         },
+        format_on_save = {
+            -- These options will be passed to conform.format()
+            timeout_ms = 500,
+            lsp_format = "fallback",
+        },
     },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -242,6 +242,7 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'prettierd', -- Used to format Javascript    
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
